afed8e236f721402a3589214477dbaac39e39d24
phanthanhhang <phanthanhhang.lhp@gmail.com>
2020-06-08 10:09:50 +0700

    first commit

---
+++ magento2/app/code/Magetop/Helloworld/Block/Index.php
@@ -0,0 +1,10 @@
+<?php
+namespace Magetop\Helloworld\Block;
+
+use Magento\Framework\View\Element\Template;
+use Magento\Widget\Block\BlockInterface;
+
+class Index extends Template
+{
+
+}
---
+++ magento2/app/code/Magetop/Helloworld/Controller/Index/Helloworld.php
@@ -0,0 +1,23 @@
+<?php
+namespace Magetop\Helloworld\Controller\Index;
+
+use Magento\Framework\App\Action\Action;
+use Magento\Framework\View\Result\PageFactory;
+use Magento\Framework\App\Action\Context;
+
+class Helloworld extends Action
+{
+    protected $pageFactory;
+
+    public function __construct(Context $context, PageFactory $pageFactory)
+    {
+        parent::__construct($context);
+        $this->pageFactory = $pageFactory;
+    }
+
+
+    public function execute()
+    {
+        return $this->pageFactory->create();
+    }
+}
